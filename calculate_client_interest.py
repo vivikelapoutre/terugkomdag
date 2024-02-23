13887149a1a202c042a5fdf4965824d638690d45
Vivike Lapoutre <v.lapoutre@gmail.com>
2024-02-23 11:12:23 +0100

    extra typefout

--- calculate_client_interest.py
+++ calculate_client_interest.py
@@ -11,14 +11,14 @@ def load_monthly_data():
 def select_clients(client_data):
     return client_data[client_data["Missing_2010"] == False]["Klantnummer"]
 
-def calculate_clients(monthly_data, selected_clients):
+def claculate_clients(monthly_data, selected_clients):
     data_to_use = monthly_data[monthly_data["Klantnummer"].isin(selected_clients)]
     return data_to_use.groupby(["Klantnummer"])["Betaalde rente"].sum()
 
 client_data = load_client_data()
 selected_clients = select_clients(client_data)
 
-monthly_data = load_monthly_data()
+monthly_daat = load_monthly_data()
 
-intreest_per_client = calculate_clients(monthly_data, selected_clients)
+intreest_per_client = claculate_clients(monthly_daat, selected_clients)
 print(intreest_per_client)
